4ae9399ea3bf4d9a0b36d899a094338967d416c3
ultravert <feodor.sax@gmail.com>
2022-06-05 15:26:37 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Fedor Oberyukhtin");
+        System.out.println("16.02.1990");
+    }
+}
