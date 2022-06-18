4f6cbb11afa6b94b8579c6af718c8e217b0481cc
ultravert <feodor.sax@gmail.com>
2022-06-18 20:32:35 +0500

    4.3.5. Множественное логическое выражение И [#9461]

---
+++ src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Triangle {
+
+    public static boolean exist(double ab, double ac, double bc) {
+        return ab + ac > bc && ab + bc > ac && ac + bc > ab;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(exist(2.0, 2.0, 2.0));
+    }
+}
