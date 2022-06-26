23443cbb56948736c60999408104429b828172e8
ultravert <feodor.sax@gmail.com>
2022-06-26 21:14:22 +0500

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        while (amount >= 0) {
+            amount = amount * percent / 100 + amount - salary;
+            year += 1;
+        }
+        return year;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(year(100, 70, 50));
+    }
+}
