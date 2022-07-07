a175b78dd6dc21970fdef92bd47c1edef2df210e
ultravert <feodor.sax@gmail.com>
2022-07-07 21:37:41 +0500

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -11,8 +11,7 @@ public class Machine {
         for (int i = 0; i < coins.length; i++) {
                 while (diff >= coins[i]) {
                     diff -= coins[i];
-                    rsl[size] = coins[i];
-                    size++;
+                    rsl[size++] = coins[i];
                 }
         }
         return Arrays.copyOf(rsl, size);
